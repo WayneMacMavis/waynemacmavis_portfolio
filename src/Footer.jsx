f93670943dc2123aac895b8752a4025471110f79
Wayne Mac Mavis <w.macmavis83@gmail.com>
2024-12-15 06:25:50 +0200

    update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
         setButtonText("Sending...");
 
         try {
-            const response = await fetch("http://http://localhost:10000/backend/contact.php", {
+            const response = await fetch("/backend/contact.php", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
