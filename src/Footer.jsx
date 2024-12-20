a89e78540c1568c40e1367507f76ed803c87d1c8
Wayne Mac Mavis <w.macmavis83@gmail.com>
2024-12-20 18:07:47 +0200

    contact form backend

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
         setButtonText("Sending...");
 
         try {
-            const response = await fetch("http://localhost:8080/contact.php", {
+            const response = await fetch("https://waynemacmavis-portfolio.onrender.com/contact.php", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
